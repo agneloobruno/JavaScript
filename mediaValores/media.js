95959c9c2f430f1568a5920a5c5acde33dd4164c
Bruno Agnelo <brunoagnelo.almeida@gmail.com>
2023-08-01 10:21:18 -0400

    media

---
+++ mediaValores/media.js
@@ -0,0 +1,11 @@
+var nota1 = 4;
+var nota2 = 7;
+var nota3 = 10;
+var media = (nota1 + nota2 + nota3)/3;
+
+if (media >=7) {
+    alert("Média: " + media + " Aprovado");
+}
+else {
+    alert("Média: " + media + " Reprovado");
+}
