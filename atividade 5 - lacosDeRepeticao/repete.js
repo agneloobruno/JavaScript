091576a1a374218658263e5a9932cf3bb136b10d
Bruno Agnelo <brunoagnelo.almeida@gmail.com>
2023-08-01 14:20:53 -0400

    add atividade 6

--- atividade 5 - lacosDeRepeticao/repete.js
+++ atividade 5 - lacosDeRepeticao/repete.js
@@ -1,5 +1,8 @@
-var repete = 1;
-while(repete <=3) {
-    alert("Mensagem: " + repete);
-    repete = repete + 1;
+var inicio = 2;
+var fim = 1000;
+var todosPares = "números pares: ";
+while(inicio <= fim) {
+    todosPares = todosPares + inicio + ", ";
+    inicio = inicio + 2;
 }
+alert(todosPares);
