6a0e6420ff9590b331900513932b58d4a5589b20
Bruno Agnelo <brunoagnelo.almeida@gmail.com>
2023-08-03 10:07:51 -0400

    added atividade 8 - eventos de click

---
+++ atividade 8 - Evento Onclick/click.js
@@ -0,0 +1,9 @@
+function umClick (){
+    document.getElementById("imagem").src="./Avatar2.png";
+    document.getElementById("log").innerHTML += "troca para o light mode<br>";
+}
+
+function doisClick() {
+    document.getElementById("imagem").src="Avatar.png";
+    document.getElementById("log").innerHTML += "troca para o dark mode<br>";
+}
